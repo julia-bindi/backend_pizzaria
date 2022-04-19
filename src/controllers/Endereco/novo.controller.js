26284a5ff63bea4c33f9eace91338f00ea9fd073
julia-bindi <juliabindi@gmail.com>
2022-04-18 21:20:32 -0300

    remove idCliente requirement from body request

--- src/controllers/Endereco/novo.controller.js
+++ src/controllers/Endereco/novo.controller.js
@@ -1,12 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
 const { EnderecoService } = require("../../services");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+const { constants } = require("../../utils");
 const yup = require("yup");
 
 module.exports = {
     novoEndereco: async (req, res) =>{
         try{
             const schema = yup.object().shape({
-                idCliente: yup.number().required(),
                 cep: yup.string().required(),
                 bairro: yup.string().required(),
                 rua: yup.string().required(),
@@ -18,8 +20,13 @@ module.exports = {
                 stripUnknown: true,
               });
 
+            const [scheme, token]  = req.headers.authorization.split(" ");
             const { idCliente, cep, bairro, rua, numero, complemento } = req.body;
-            const response = await EnderecoService.novoEndereco(idCliente, cep, bairro, rua, numero, complemento);
+
+            const verify = promisify(jwt.verify);
+            const logged_user = await verify(token, constants.jwtToken);
+            
+            const response = await EnderecoService.novoEndereco(logged_user.id, cep, bairro, rua, numero, complemento);
             return res.status(StatusCodes.OK).json(response);
         }catch (error) {
             console.error(error);
