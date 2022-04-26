3ab79f3bec17bd846c4f5b89cf78cc11d2121067
julia-bindi <juliabindi@gmail.com>
2022-04-26 10:24:32 -0300

    add cors package

--- src/routes/cliente.route.js
+++ src/routes/cliente.route.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const cors = require("cors")
 const { ClienteController } = require("../controllers");
 
+const corsoptions = {
+    origin: "*",
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    optionsSuccessStatus: 200
+}
+
 router.post("/login", ClienteController.login)
-router.post("/novo", ClienteController.novo)
+router.post("/novo",cors(corsoptions), ClienteController.novo)
 
 module.exports.cliente = router;
