e53215d5d7bd9bbc56fc31da36bf22434e26531f
julia-bindi <juliabindi@gmail.com>
2022-04-29 19:19:14 -0300

    add options request

--- src/routes/cliente.route.js
+++ src/routes/cliente.route.js
@@ -1,15 +1,18 @@
 const router = require("express").Router();
+const { StatusCodes } = require("http-status-codes");
 const cors = require("cors");
 const { ClienteController } = require("../controllers");
 
 const corsoptions = {
     origin: "*",
-    methods: ['GET', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type','Authorization'],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization', 'Content-Length','X-Requested-With'],
     optionsSuccessStatus: 200
   }
 
+router.options("/login", cors(corsoptions), async (req, res) =>{ return res.status(StatusCodes.OK)})
 router.post("/login", cors(corsoptions), ClienteController.login)
+router.options("/novo", cors(corsoptions), async (req, res) =>{ return res.status(StatusCodes.OK)})
 router.post("/novo", cors(corsoptions), ClienteController.novo)
 
 module.exports.cliente = router;
