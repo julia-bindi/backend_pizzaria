1123598374708e2b5166e02d14308f6cdcf68e2a
julia-bindi <juliabindi@gmail.com>
2022-04-30 13:42:22 -0300

    test routes options

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,13 @@ const { cliente } = require("./cliente.route")
 const { endereco } = require("./endereco.route")
 const { produto } = require("./produto.route")
 const { pedido } = require("./pedido.route")
-const enderecoOPT = require("./enderecoOptions.route")
-const pedidoOPT = require("./PedidoOptions.route")
+const { endereco } = require("./enderecoOptions.route")
+const { pedido } = require("./PedidoOptions.route")
 
 module.exports = {
     hello,
     cliente,
     endereco,
     produto,
-    pedido,
-    enderecoOPT,
-    pedidoOPT
+    pedido
 }
